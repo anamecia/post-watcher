cb2f7a7d76f5005a7955289ebf034908b0c0ec6b
anamecia <anameciarobalo@gmail.com>
2025-03-10 21:42:13 +0000

    setup drizzle and turso db

---
+++ db/schema.ts
@@ -0,0 +1,19 @@
+import { sql } from 'drizzle-orm'
+import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { randomUUID } from 'node:crypto'
+
+const id = () =>
+  text('id')
+    .primaryKey()
+    .$default(() => randomUUID())
+const createdAt = () =>
+  text('created_at')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull()
+
+export const users = sqliteTable('users', {
+  id: id(),
+  createdAt: createdAt(),
+  email: text('email').unique().notNull(),
+  password: text('password').notNull(),
+})
